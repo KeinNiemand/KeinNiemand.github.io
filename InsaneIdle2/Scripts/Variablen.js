a43310082855e3f2e339d65f557709c5a5247ef9
Lukas <Lukas@DESKTOP-LC43BCA.fritz.box>
2016-10-29 13:31:41 +0200

    Insane Idle 2 Update Alpha 1.1.0

--- InsaneIdle2/Scripts/Variablen.js
+++ InsaneIdle2/Scripts/Variablen.js
@@ -39,10 +39,10 @@ for (i = 1; i <= anzahl; i++) {
     Game.prosek[i] = Decimal(0)
     //Game.basPreis[i] = Decimal(Math.pow(1+((i*0.1)-0.2), 66.66) * 100)
     //BASIS PREIS FORMEL
-    Game.basPreis[i] = Decimal(Decimal.pow(Decimal.mul(i,0.1).sub(0.2).add(1), 80.66).mul(2))
+    Game.basPreis[i] = Decimal(Decimal.pow(Decimal.mul(i,0.1).sub(0.2).add(1), 186.66).mul(2))
     //Game.preiserh[i] = Decimal(1+(1.5*i)^1.8)
     //PREIS ERHÖUNGS FORMEL
-    Game.preiserh[i] = Decimal(Decimal.mul(1.2,i).pow(1.6).add(1))
+    Game.preiserh[i] = Decimal(Decimal.mul(1.1,i).pow(1.7).add(1))
     Game.preis[i] = Game.basPreis[i]
     Game.produmul[i] = Decimal(0.0001)
 }
