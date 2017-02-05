bde0afca6bccc9c6c1e211f32a7d34d13b93d7a5
Lukas <Lukas@DESKTOP-LC43BCA.fritz.box>
2017-02-05 18:20:50 +0100

    Insane Idle 2 A2 Test fix save

--- InsaneIdle2/A2/Scripts/Speichern.js
+++ InsaneIdle2/A2/Scripts/Speichern.js
@@ -5,7 +5,7 @@
  */
 var combineObj = function(obj1, obj2) {
     for(var key in obj2){
-        if (typeof obj2[key] !== "object" || obj1[key] === undefined)
+        if (typeof obj2[key] !== "object" ||  typeof obj1[key] === "undefined")
         obj1[key] = obj2[key]
         else {
             for (var i = 0; i<obj2[key].length; i++) {
